ace9f629ac2261fa20362ede01982042fe08326b
Michael Fenwick <mickyfen17@aol.com>
2017-08-30 23:40:58 -0600

    add basic first glamorous styling to Title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import glamorous from 'glamorous';
 import './App.css';
+
+const Title = glamorous.h1({
+  fontSize: '2em',
+  color: 'red',
+  textAlign: 'center',
+  padding: 10,
+});
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Default Header</h1>
+        <Title>TESTING THE TITLE WITH GLAMOROUS</Title>
       </div>
     );
   }

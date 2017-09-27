4eb60fce05ffa4cb895e39eaf7d6ab87f118cda6
Michael Fenwick <mickyfen17@aol.com>
2017-09-26 19:38:36 -0600

    finish styling for boltHeads

--- src/Components/Bolts.js
+++ src/Components/Bolts.js
@@ -3,27 +3,29 @@ import glamorous from 'glamorous';
 
 const BoltWrapper = glamorous.div({
   position: 'absolute',
-  bottom: '210px',
+  bottom: '220px',
   display: 'flex',
   justifyContent: 'space-between',
   width: '200px',
 });
 
 const BoltHeadWrapper = glamorous.div(props => ({
-  width: '15px',
+  width: '18px',
   margin: props.right ? '0 10px 0 0' : '0 0 0 10px',
 }));
 
-const BoltHeadMiddle = glamorous.div(props => ({
+const BoltHeadMiddle = glamorous.div({
+  position: 'relative',
   height: '20px',
   background: '#d0dcbc',
   borderRadius: '4px',
-}));
+});
 
 const BoltHeadOuter = glamorous.div(props => ({
   height: '12px',
   background: props.bottom ? '#b2be9e' : '#e4f0d0',
   borderRadius: '4px',
+  margin: props.top ? '0 0 -6px 0' : '-6px 0 0 0',
 }));
 
 class Bolts extends Component {
